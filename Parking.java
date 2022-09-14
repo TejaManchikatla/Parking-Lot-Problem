5889e343459b789c3ab190067f11ef9aec9e96f0
Manchikatla Teja <cs21b031@iittp.ac.in>
2022-09-14 23:13:50 +0530

    tejatry

---
+++ Parking.java
@@ -0,0 +1,7 @@
+public class Parking {
+    public static void main(String[] args){
+        System.out.println("Good");
+        System.out.println("Project");
+        System.out.println("Thing");
+    }
+}
