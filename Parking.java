e49ae64664dd38f8cdc32c61836161eebf3e5e96
manchikatla-teja <69082955+manchikatla-teja@users.noreply.github.com>
2022-09-17 15:26:36 +0530

    Made ground floor and number of floors
    Created an array of objects to represent the normal floors.
    A single object is made to represent the ground floor.

--- Parking.java
+++ Parking.java
@@ -14,6 +14,28 @@ abstract class Floor{
     abstract void update(); // updates the counter of vehicle that exits/enters
 }
 
+class Ground_Floor extends Floor{
+    int current_capacity=0;
+    int max_capacity = 100;
+    int four_wheelers = 0;
+    int max_four_wheelers = 60;
+    int two_wheelers = 0;
+    int max_two_wheelers = 40;
+    int number_of_4W_EV_slots_available = 30;
+    int number_of_2W_EV_slots_available = 30;
+
+    void display_board(){
+        System.out.println("Total Empty Slots: " + (max_capacity - current_capacity));
+        System.out.println("Total Empty 4W Slots: " + (this.max_four_wheelers - this.four_wheelers));
+        System.out.println("Total Empty 2W Slots: " + (this.max_two_wheelers - this.two_wheelers));
+        System.out.println("No. of 4W EV slots available: " + number_of_4W_EV_slots_available);
+        System.out.println("No. of 2W EV slots available: " + number_of_2W_EV_slots_available);
+    }
+    void exit(){};
+    void payment(){};
+    void update(){};
+}
+
 class Normal_Floor extends Floor{
     int current_capacity=0;
     int max_capacity = 100;
@@ -24,16 +46,39 @@ class Normal_Floor extends Floor{
 
     void display_board(){
         System.out.println("Total Empty Slots: " + (this.max_capacity - this.current_capacity));
-        System.out.println("Total Empty Slots: " + (this.max_four_wheelers - this.four_wheelers));
-        System.out.println("Total Empty Slots: " + (this.max_two_wheelers - this.two_wheelers));
+        System.out.println("Total Empty 4W Slots: " + (this.max_four_wheelers - this.four_wheelers));
+        System.out.println("Total Empty 2W Slots: " + (this.max_two_wheelers - this.two_wheelers));
     }
-
+    void exit(){};
+    void payment(){};
+    void update(){};
+    
 }
 
 
 
 public class Parking {
     public static void main(String[] args){
-        System.out.println("ParkingLot");
+        int i;
+
+        // array to store the objects of normal-floors
+        Normal_Floor[] floors = new Normal_Floor[5]; //building of 1 ground and 5 normal floors
+
+        // object for ground floor
+        Ground_Floor ground = new Ground_Floor();
+
+        //Initializing things for each floor(creating objects of floors)
+        for(i=0; i<5; i++) floors[i] = new Normal_Floor();
+
+        //displaying ground-floor data
+        System.out.println("-----  Ground Floor   -----");
+        ground.display_board();
+
+        //running a loop to check data on every floor
+        for(i=0; i<5; i++){
+            System.out.println("-----  Floor ("+i+1+")   -----"); // prints the floor number
+            floors[i].display_board(); // its data
+        }
+
     }
 }
